97f7ce53003ac0361865e9694e243d6bf0c1cdb0
Kair0s3 <stianboon@gmail.com>
2022-03-10 19:32:09 +0800

    Testing for loops to create NuxtLink.

--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,28 @@
 <template>
   <div>
     <h1>Home Page</h1>
+    <!--Testing dynamic folder routing in Nuxt-->
     <div>
       <NuxtLink to="/Campaign/123/info">
         View Campaign at address - {{testCampaignAddress}}
       </NuxtLink>
     </div>
+    <!--Testing dynamic file routing in Nuxt-->
     <div>
       <NuxtLink to="/User/456">
         View user at address - {{testUserAddress}}
       </NuxtLink>
+    </div>
+    <!--Testing for loop creation in Nuxt-->
+    <div>
+        <p>
+          Testing for loop for creating nuxtlinks
+        </p>
+        <div v-for="userAddress in testUserAddressList">
+          <NuxtLink :to="`/User/${userAddress}`">
+            Link to User {{userAddress}}
+          </NuxtLink>
+        </div>
     </div>
   </div>
 </template>
@@ -20,7 +33,15 @@ export default {
   data () {
     return {
       testCampaignAddress: 123,
+      testUserAddressList: [
+        1, 2, 3, 4, 5, 6
+      ],
       testUserAddress: 456
+    }
+  },
+  methods: {
+    getCurrentUserAddress () {
+
     }
   }
 }
